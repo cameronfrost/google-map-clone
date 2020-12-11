e00da0fc94a961a4545d2e9675eb2d6cc762d357
Cameron <c@cameronfrost.com>
2020-12-11 15:48:58 -0600

    Add location and default location

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiY2FtZXJvbmZyb3N0IiwiYSI6ImNraWtzNXNucjBjd2oyenN2eDFnOWU5YTEifQ.pe_0wM6cPgw2DcMQQ7ItSg";
 
-var map = new mapboxgl.Map({
-  accessToken: MAPBOX_ACCESS_TOKEN,
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-});
-
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
   enableHighAccuracy: true,
 });
 
-function setupMap(centerPosition) {}
+function setupMap(centerPosition) {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: centerPosition,
+    zoom: 15,
+  });
+}
 
 function successLocation(position) {
   setupMap([position.coords.longitude, position.coords.latitude]);
 }
 
-function errorLocation() {}
+function errorLocation() {
+  setupMap([-2.24, 53.48]);
+}
